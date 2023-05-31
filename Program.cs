5687b372db3241cd9574eec99092e0e15e7c64a3
filip2cz <filda.komarek@seznam.cz>
2023-05-31 14:53:13 +0200

    Přidejte soubory projektu.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Net;
+
+string url = "https://v4v6.ipv6-test.com/api/myip.php";
+
+bool i = true;
+while (i)
+{
+    using (WebClient client = new WebClient())
+    {
+        try
+        {
+            string response = client.DownloadString(url);
+            Console.WriteLine(response);
+            i = false;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Chyba při získávání dat: " + ex.Message);
+        }
+    }
+    Console.WriteLine("http request done");
+}
